875260b66cad24268010082f20c4a2c3b8c51b78
gaurav632007 <gauravagrawal2030@gmail.com>
2025-11-25 13:47:11 +0530

    photo testing

--- pages/About.py
+++ pages/About.py
@@ -21,3 +21,16 @@ md ='''
 '''
 
 st.markdown(md)
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("Achintya Mishra")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("Gaurav Agrawal ")
+    st.image("app/static/p.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
