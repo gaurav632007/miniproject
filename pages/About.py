d1c2000968c71ce5e5da47ce4d9b231f21e9a22f
gaurav632007 <gauravagrawal2030@gmail.com>
2025-11-26 10:53:32 +0530

    Set image width to stretch for team members

--- pages/About.py
+++ pages/About.py
@@ -17,24 +17,24 @@ c1, c2, c3, c4, c5 , c6 = st.columns(6)
 
 with c1:
     st.text("Achintya Mishra")
-    st.image("static/a.jpg")
+    st.image("static/a.jpg",width = "stretch")
 
 with c2:
     st.text("Arnav Kohli")
-    st.image("static/arnav.jpg")
+    st.image("static/arnav.jpg",width= "stretch")
 
 with c3:
     st.text("Dhanvin Ambavkar")
-    st.image("static/dhanvin.jpg")
+    st.image("static/dhanvin.jpg", width = "stretch")
 
 with c4:
     st.text("Ayaan Lone")
-    st.image("static/ayaan.jpg")
+    st.image("static/ayaan.jpg", width = "stretch")
 
 with c5:
     st.text("Eeshaan Suryawanshi")
-    st.image("static/eeshaan.jpg")
+    st.image("static/eeshaan.jpg", width = "stretch")
 with c6:
     st.text("Gaurav Agrawal")
-    st.image("static/gaurav.jpg")
+    st.image("static/gaurav.jpg", width = "stretch")
 
