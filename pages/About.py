d37db0523577c76c5d1991011fac99cdd64d4dd2
gaurav632007 <gauravagrawal2030@gmail.com>
2025-11-26 11:07:43 +0530

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -13,28 +13,32 @@ md ='''
 '''
 
 st.markdown(md)
-c1, c2, c3, c4, c5 , c6 = st.columns(6,vertical_alignment= "top")
+
+c1, c2, c3 = st.columns(3, vertical_alignment="center")
 
 with c1:
     st.markdown("###### Achintya Mishra")
-    st.image("static/a.jpg",width = "stretch")
+    st.image("static/a.jpg", width="stretch")
 
 with c2:
     st.markdown("###### Arnav Kohli")
-    st.image("static/arnav.jpg",width= "stretch")
+    st.image("static/arnav.jpg", width="stretch")
 
 with c3:
     st.markdown("###### Dhanvin Ambavkar")
-    st.image("static/dhanvin.jpg", width = "stretch")
+    st.image("static/dhanvin.jpg", width="stretch")
+
+
+c4, c5, c6 = st.columns(3, vertical_alignment="center")
 
 with c4:
     st.markdown("###### Ayaan Lone")
-    st.image("static/ayaan.jpg", width = "stretch")
+    st.image("static/ayaan.jpg", width="stretch")
 
 with c5:
     st.markdown("###### Eeshaan Suryawanshi")
-    st.image("static/eeshaan.jpg", width = "stretch")
+    st.image("static/eeshaan.jpg", width="stretch")
+
 with c6:
     st.markdown("###### Gaurav Agrawal")
-    st.image("static/gaurav.jpg", width = "stretch")
-
+    st.image("static/gaurav.jpg", width="stretch")
