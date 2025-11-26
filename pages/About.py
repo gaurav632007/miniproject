aa2ad2dd3364f204fea6c800ac9a20192d376ed3
gaurav632007 <gauravagrawal2030@gmail.com>
2025-11-26 10:56:24 +0530

    Change text to markdown for team member names

--- pages/About.py
+++ pages/About.py
@@ -13,28 +13,28 @@ md ='''
 '''
 
 st.markdown(md)
-c1, c2, c3, c4, c5 , c6 = st.columns(6)
+c1, c2, c3, c4, c5 , c6 = st.columns(6,vertical_alignment= "top")
 
 with c1:
-    st.text("Achintya Mishra")
+    st.markdown("### Achintya Mishra")
     st.image("static/a.jpg",width = "stretch")
 
 with c2:
-    st.text("Arnav Kohli")
+    st.markdown("###Arnav Kohli")
     st.image("static/arnav.jpg",width= "stretch")
 
 with c3:
-    st.text("Dhanvin Ambavkar")
+    st.markdown("###Dhanvin Ambavkar")
     st.image("static/dhanvin.jpg", width = "stretch")
 
 with c4:
-    st.text("Ayaan Lone")
+    st.markdown("###Ayaan Lone")
     st.image("static/ayaan.jpg", width = "stretch")
 
 with c5:
-    st.text("Eeshaan Suryawanshi")
+    st.markdown("###Eeshaan Suryawanshi")
     st.image("static/eeshaan.jpg", width = "stretch")
 with c6:
-    st.text("Gaurav Agrawal")
+    st.markdown("###Gaurav Agrawal")
     st.image("static/gaurav.jpg", width = "stretch")
 
