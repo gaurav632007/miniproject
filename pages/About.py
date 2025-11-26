a836a8aaabdb5a1af99e843b502a910ba7264827
gaurav632007 <gauravagrawal2030@gmail.com>
2025-11-26 10:57:40 +0530

    Change headings from H3 to H4 for team members

--- pages/About.py
+++ pages/About.py
@@ -16,25 +16,25 @@ st.markdown(md)
 c1, c2, c3, c4, c5 , c6 = st.columns(6,vertical_alignment= "top")
 
 with c1:
-    st.markdown("### Achintya Mishra")
+    st.markdown("#### Achintya Mishra")
     st.image("static/a.jpg",width = "stretch")
 
 with c2:
-    st.markdown("###Arnav Kohli")
+    st.markdown("#### Arnav Kohli")
     st.image("static/arnav.jpg",width= "stretch")
 
 with c3:
-    st.markdown("###Dhanvin Ambavkar")
+    st.markdown("#### Dhanvin Ambavkar")
     st.image("static/dhanvin.jpg", width = "stretch")
 
 with c4:
-    st.markdown("###Ayaan Lone")
+    st.markdown("#### Ayaan Lone")
     st.image("static/ayaan.jpg", width = "stretch")
 
 with c5:
-    st.markdown("###Eeshaan Suryawanshi")
+    st.markdown("#### Eeshaan Suryawanshi")
     st.image("static/eeshaan.jpg", width = "stretch")
 with c6:
-    st.markdown("###Gaurav Agrawal")
+    st.markdown("#### Gaurav Agrawal")
     st.image("static/gaurav.jpg", width = "stretch")
 
