4de7f884ad26672707af75a401e070313e5c4343
gaurav632007 <gauravagrawal2030@gmail.com>
2025-11-26 07:38:11 +0530

    added agriculture

--- pages/Agricultural%in GDP.py
+++ pages/Agricultural%in GDP.py
@@ -10,7 +10,7 @@ st.set_page_config(
 
 
 ys = [str(y) for y in range(1960, 2025)]
-df = pd.read_csv("datasets/agricultural_percent_of_gdp.csv")
+df = pd.read_csv("datasets/agriculture_percent_of_gdp.csv")
 long = pd.melt(df, id_vars=['geo', 'name'], var_name='year', value_name='value')
 long['geo'] = long['geo'].str.upper()
 
